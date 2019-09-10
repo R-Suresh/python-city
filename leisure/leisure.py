284dc6a095f516dcd2f5a30d9866be3acdf24577
R-Suresh <26441234+R-Suresh@users.noreply.github.com>
2019-09-10 04:47:28 -0400

    Added stadium features

--- leisure/leisure.py
+++ leisure/leisure.py
@@ -1,7 +1,13 @@
-from .museum import draw_museum
-from .restaurant import draw_restaurant
+from museum import draw_museum
+from restaurant import draw_restaurant
+from stadium import draw_stadium_parking
+
 
 def draw_leisure():
     draw_museum()
     draw_restaurant()
+    draw_stadium_parking(5, 50, 8)
     return
+
+
+draw_leisure()
--- leisure/stadium.py
+++ leisure/stadium.py
@@ -1,7 +1,59 @@
-from .museum import draw_museum
-from .restaurant import draw_restaurant
+def draw_stadium_parking(stadium_height, stadium_width, length):
+    draw_stadium(stadium_height, stadium_width)
+    draw_parkinglot(length)
 
-def draw_leisure():
-    draw_museum()
-    draw_restaurant()
-    return
+
+def draw_stadium(stadium_height=5, stadium_width=50):
+    """
+    Prints a stadium of given parameters:
+
+    args:
+        stadium_height: Height of the rectangular stadium
+        stadium_width : Width of the rectangular stadium
+    """
+
+    draw_width = " "
+    draw_height = ""
+
+    for i in range(stadium_width):
+        draw_width += "-"
+
+    draw_width += "\n"
+
+    for i in range(stadium_height):
+        draw_height += "|"
+        for j in range(stadium_width + 1):
+            draw_height += " "
+
+        draw_height += "|\n"
+
+    stadium = draw_width + draw_height + draw_width
+
+    print()
+    print(stadium)
+    print()
+
+
+def print_a_cell():
+    print("[ ]", end="")
+
+
+def draw_parkinglot(length=5):
+    """
+    Prints a road of a given lenth:
+
+    args:
+        length: integer define the length of the parking lot.
+    """
+
+    # sanitizing parameters
+    if length < 0:
+        raise ValueError("length is negative")
+    if length == 0:
+        return
+
+    # Drawing Cells
+    for i in range(length):
+        for j in range(length):
+            print_a_cell()
+        print("")
